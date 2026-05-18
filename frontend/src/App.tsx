efb128513a262b581c974264085621a9138d8dfb
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2026-05-18 17:28:04 +0700

    feat(frontend): add pages setup and fix dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,40 @@
-import { useEffect, useState } from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
-import { api } from "./api/api";
-
-import type { UserResponse } from "./types/api";
-
-import UserCard from "./components/users/UserCard";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
 
-    const [users, setUsers] = useState<UserResponse[]>([]);
-
-    useEffect(() => {
-
-        api.get("/users")
-            .then(response => {
-
-                setUsers(response.data);
-
-            });
-
-    }, []);
-
     return (
 
-        <div className="min-h-screen bg-gray-100 p-8">
+        <BrowserRouter>
 
-            <h1 className="text-5xl font-bold text-purple-600 mb-8">
-                Users
-            </h1>
+            <div className="p-4 bg-black text-white flex gap-4">
 
-            <div className="grid gap-4">
+                <Link to="/users">
+                    Users
+                </Link>
 
-                {users.map(user => (
-
-                    <UserCard
-                        key={user.id}
-                        user={user}
-                    />
-
-                ))}
+                <Link to="/bands">
+                    Bands
+                </Link>
 
             </div>
-        </div>
+
+            <Routes>
+
+                <Route
+                    path="/users"
+                    element={<UsersPage />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
     );
 }
 
